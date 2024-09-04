04514a20667d4a2659ec90063a6bccb5faf0b184
Some Wallet <somewallet@gmail.com>
2024-09-05 00:38:47 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
 import {AllWallets} from "./wallets";
 import {THEME, TonConnectButton, TonConnectUIProvider} from "@tonconnect/ui-react";
 export default function Home() {
-    return ( <main className="mt-5 min-w-full flex flex-col items-center bg-black">
+    return ( <main className="min-w-full min-h-screen flex flex-col items-center bg-black text-white">
         <TonConnectUIProvider manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
             uiPreferences={{theme:THEME.DARK}} 
             walletsListConfiguration={{includeWallets:AllWallets}} 
             actionsConfiguration={{twaReturnUrl:"https://t.me/tonrotatebot/start"}} >
-        <TonConnectButton />
+        <TonConnectButton className="mt-5" />
         <h1 className="mt-10">Hello World</h1> </TonConnectUIProvider> </main> ); }
